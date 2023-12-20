21abfad28d52eeefb5cdd462f8ab8b4ff5de9e61
bobosneefdev <bobosneefbusiness@gmail.com>
2023-12-20 09:56:15 -0800

    small logger fix

--- common/functions/sd_logger.js
+++ common/functions/sd_logger.js
@@ -1,8 +1,10 @@
 import { getTime } from "./sd_utility.js";
 
-export function sdLogger(message, dividerStr = null) {
-    if (dividerStr != null) {
-        console.log(`${dividerStr}`);
+export function sdLogger(message, divider = null) {
+    if (divider == true) {
+        console.log(`--------------------------------------------------`);
+    } else if (divider) {
+        console.log(`${divider}`);
     }
     console.log(`[${getTime()}] : ${message}`);
 }
